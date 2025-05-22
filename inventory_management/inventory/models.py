05995bab07878af5b1a5aa9a5b21de9cc40a6258
NicollyMendes <146892975+NicollyMendes@users.noreply.github.com>
2025-05-22 11:35:16 -0400

    Update models.py
    Adicionei a tabela Clientes

--- inventory_management/inventory/models.py
+++ inventory_management/inventory/models.py
@@ -20,6 +20,19 @@ class Categoria(models.Model):
     def __str__(self):
         return self.nome
 
+class Cliente(models.Model):
+    nome = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=14, unique=True)
+    email = models.EmailField(blank=True, null=True)
+    telefone = models.CharField(max_length=20, blank=True, null=True)
+    endereco = models.CharField(max_length=150, blank=True, null=True)
+    cidade = models.CharField(max_length=50, blank=True, null=True)
+    estado = models.CharField(max_length=2, blank=True, null=True)
+    data_cadastro = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.nome} - {self.cpf}"
+
 
 class Venda(models.Model):
     cliente = models.ForeignKey('Cliente', on_delete=models.CASCADE)
